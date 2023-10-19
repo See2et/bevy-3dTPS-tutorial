6974d41357ae7064e710e3487b83fdfcca3c20c2
see2et <seezet1024@gmail.com>
2023-10-19 20:36:06 +0900

    make floor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_floor)
+        .run();
+}
+
+fn spawn_floor(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let floor = PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane::from_size(15.0))),
+        material: materials.add(Color::DARK_GREEN.into()),
+        ..default()
+    };
+
+    commands.spawn(floor);
 }
